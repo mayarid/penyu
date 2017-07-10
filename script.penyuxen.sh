5682a25e31129dee413e3286d5440be1f21efb61
anak10thn <anak10thn@gmail.com>
2017-07-10 12:54:35 +0700

    CHANGE: penyu xen section

--- script.penyuxen.sh
+++
@@ -1,8 +0,0 @@
-build_penyuxen() {
-	apk fetch --root "$APKROOT" --stdout xen-hypervisor | tar -C "$DESTDIR" -xz boot
-}
-
-section_penyuxen() {
-	[ -n "${xen_params+set}" ] || return 0
-	build_section penyuxen $ARCH $(apk fetch --root "$APKROOT" --simulate xen-hypervisor | checksum)
-}
