b20c75ff084f3c731617282ab280b8d316dc321e
anak10thn <anak10thn@gmail.com>
2017-07-10 12:27:26 +0700

    CHANGE: penyu xen name

--- penyuxen.conf.mk
+++ penyuxen.conf.mk
@@ -2,4 +2,4 @@ PENYU_NAME     := penyu
 PENYU_TYPE			:= xen
 MODLOOP_EXTRA   := zfs spl
 KERNEL_FLAVOR   := hardened
-PENYU_OVL				:= genapkovl-penyu-xen.sh
+PENYU_OVL				:= genapkovl-penyuxen.sh
--- script.penyuxen.sh
+++ script.penyuxen.sh
@@ -1,8 +1,8 @@
-build_penyu-xen() {
+build_penyuxen() {
 	apk fetch --root "$APKROOT" --stdout xen-hypervisor | tar -C "$DESTDIR" -xz boot
 }
 
-section_penyu-xen() {
+section_penyuxen() {
 	[ -n "${xen_params+set}" ] || return 0
 	build_section xen $ARCH $(apk fetch --root "$APKROOT" --simulate xen-hypervisor | checksum)
 }
