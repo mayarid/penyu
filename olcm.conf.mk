20821a367259ced275cc992c71f4e3abf46af94c
anak10thn <anak10thn@gmail.com>
2017-07-19 23:41:19 +0700

    UPDATE: travis ci

--- olcm.conf.mk
+++ olcm.conf.mk
@@ -1,5 +1,5 @@
 PENYU_NAME     := penyu
-PENYU_TYPE			:= base
+PENYU_TYPE			:= olcm
 MODLOOP_EXTRA   := zfs spl
 KERNEL_FLAVOR   := hardened
-PENYU_OVL				:= genapkovl-penyu.sh
+PENYU_OVL				:= genapkovl-olcm.sh
